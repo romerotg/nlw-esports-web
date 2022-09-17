f38198662f789378b0e85bc5ef08374d6d7c79a0
Romero Gonçalves <romerotg@gmail.com>
2022-09-17 13:23:20 -0300

    styling the voice channel checkbox

--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { GameController } from 'phosphor-react'
-import { CreateAdBanner } from './CreateAdBanner';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { GameController, Check } from 'phosphor-react'
 import { Input } from './Form/Input';
 
 export function CreateAdModal() {
@@ -95,7 +95,11 @@ export function CreateAdModal() {
               </div>
 
               <div className="mt-2 flex gap-2 text-sm">
-                <Input type="checkbox" />
+                <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+                    <Checkbox.Indicator>
+                        <Check className="w-4 h-4 text-emerald-400" />
+                    </Checkbox.Indicator>
+                </Checkbox.Root>
                 Costumo me conectar ao chat de voz
               </div>
 
